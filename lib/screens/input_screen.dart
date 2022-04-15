93dc14a3f284596cb5f6180bc69188ae6e12ee46
dicapisar <dicapisar@gmail.com>
2022-04-15 18:13:58 -0500

    Creación de imput_screen

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -17,77 +17,78 @@ class InputScreen extends StatelessWidget {
     };
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Text Inputs and Forms'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Form(
-              key: myFormKey,
-              child: Column(
-                children: [
-                  CustomInputField(
-                    labelText: 'Nombre',
-                    hintText: 'Nombre del Usuario',
-                    formProperty: 'first_name',
-                    formValues: formValues,
+      appBar: AppBar(
+        title: const Text('Text Inputs and Forms'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                CustomInputField(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre del Usuario',
+                  formProperty: 'first_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomInputField(
+                  labelText: 'Apellido',
+                  hintText: 'Apellido del Usuario',
+                  formProperty: 'last_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomInputField(
+                  labelText: 'Email',
+                  hintText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                  icon: Icons.email_outlined,
+                  formProperty: 'email',
+                  formValues: formValues,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomInputField(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña',
+                  keyboardType: TextInputType.text,
+                  icon: Icons.security_outlined,
+                  obscureText: true,
+                  formProperty: 'password',
+                  formValues: formValues,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: Center(child: Text('Guardar')),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  CustomInputField(
-                    labelText: 'Apellido',
-                    hintText: 'Apellido del Usuario',
-                    formProperty: 'last_name',
-                    formValues: formValues,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  CustomInputField(
-                    labelText: 'Email',
-                    hintText: 'Email',
-                    keyboardType: TextInputType.emailAddress,
-                    icon: Icons.email_outlined,
-                    formProperty: 'email',
-                    formValues: formValues,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  CustomInputField(
-                    labelText: 'Contraseña',
-                    hintText: 'Contraseña',
-                    keyboardType: TextInputType.text,
-                    icon: Icons.security_outlined,
-                    obscureText: true,
-                    formProperty: 'password',
-                    formValues: formValues,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  ElevatedButton(
-                    child: const SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: Center(child: Text('Guardar')),
-                    ),
-                    onPressed: () {
-                      FocusScope.of(context).requestFocus(FocusNode());
-                      if (!myFormKey.currentState!.validate()) {
-                        print('formulario no valido');
-                        return;
-                      }
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (!myFormKey.currentState!.validate()) {
+                      print('formulario no valido');
+                      return;
+                    }
 
-                      print(formValues);
-                    },
-                  )
-                ],
-              ),
+                    print(formValues);
+                  },
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
